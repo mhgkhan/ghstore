c08ed34bc7d31f5d03d26fc9e61c589d9b6319ab
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-12-15 15:25:38 +0500

    customize layout

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        this is footer 
+       <div className="container">
+        <p>Copyright&copy; Allright Reserved 2024.</p>
+       </div>
     </footer>
   )
 }
