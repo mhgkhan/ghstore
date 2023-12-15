fcb4670e7c84d5a48897f20fe615bbf540bb2eaf
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-12-15 16:16:03 +0500

    Update Intro.jsx

--- components/ui/pages/Intro.jsx
+++ components/ui/pages/Intro.jsx
@@ -5,9 +5,8 @@ const Intro = () => {
         <section className='intro'>
             <div className="container">
                 <div className="text-intro">
-                    <p>All You</p>
-                    <h1>Need for to  <br /> <span>solve</span> your <span>problem's</span> is </h1>
-                    <p>Here</p>
+                    <p>All Your</p>
+                    <h1>Local technical  <br /> <span>problems</span> are <span>solved</span> here </h1>
                     <div className="buttons_intro">
                         <button className='default_button'>Latest Jobs </button>
                         <button className='default_button'>Formats </button>
